90ab6457dab89b836a9d0fb1f79b254256d5bb0f
isblackout <dnnxzz70@gmail.com>
2024-02-23 22:55:40 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   "aula 56 - Fun\303\247\303\265es f\303\241brica (Factory Functions)/index.js"

--- aula 56 - Funções fábrica (Factory Functions)/index.js
+++ aula 56 - Funções fábrica (Factory Functions)/index.js
@@ -1,15 +1,33 @@
-function getPerson(name, lastName) {
+// Factory Function
+//Constructor Function
+function getPerson(name, lastName, weight, height) {
     return {
         name,
         lastName,
-        say: function(topic) {
+        get nameComplete() {
+            return `${this.name} ${this.lastName}`;
+        },
+        // Setter
+        set nameComplete(value) {
+            value = value.split(' ')
+            this.name = value.shift();
+            this.lastName = value.join(' ')
+        },
+        say(topic) {
             return `${this.name} is saying about ${topic}`
+        },
+        weight: weight,
+        height: height,
+        //Getter
+        get imc() {
+            const imc = (this.weight / (this.height ** 2));
+            return `Imc: ${imc.toFixed(2).replace('.', ',')}`;
         }
     }
 }
 
-const p1 = getPerson('John', 'Doe');
+const p1 = getPerson('John', 'Doe', 69, 1.82);
 const p2 = getPerson('Lucas', 'Max');
-
-console.log(p1.say('Joan'))
-console.log(p2.say('Maria'))
+p1.nameComplete = 'Maria Oliveira Silva'
+console.log(p1.nameComplete);
+console.log(p2.say('Maria'));
