96d303d5ecb663059a09d8391144227a54363540
isblackout <dnnxzz70@gmail.com>
2024-08-10 03:33:00 -0300

    Changes to be committed: 	modified:   Projeto Agenda/frontend/main.js 	new file:   Projeto Agenda/frontend/modules/Login.js 	modified:   Projeto Agenda/package-lock.json 	modified:   Projeto Agenda/package.json 	modified:   Projeto Agenda/routes.js 	modified:   Projeto Agenda/server.js 	new file:   Projeto Agenda/src/controllers/contatoController.js 	modified:   Projeto Agenda/src/controllers/homeController.js 	modified:   Projeto Agenda/src/controllers/loginController.js 	modified:   Projeto Agenda/src/middlewares/middleware.js 	new file:   Projeto Agenda/src/models/ContatoModel.js 	modified:   Projeto Agenda/src/models/LoginModel.js 	new file:   Projeto Agenda/src/views/contato.ejs 	modified:   Projeto Agenda/src/views/includes/nav.ejs 	modified:   Projeto Agenda/src/views/index.ejs 	new file:   Projeto Agenda/src/views/login-logado.ejs 	modified:   Projeto Agenda/src/views/login.ejs

--- Projeto Agenda/frontend/main.js
+++ Projeto Agenda/frontend/main.js
@@ -1,6 +1,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
 import './assets/css/style.css';
-
-console.log('Olá mundo 3');
+import Login from './modules/Login';
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+login.init();
+cadastro.init();
